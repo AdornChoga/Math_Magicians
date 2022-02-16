d45ce2a4271666a4417e34af95ddead64b9c0fa7
asanchy <sanchyclips@gmail.com>
2022-02-16 08:20:20 +0200

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
 
 // eslint-disable-next-line react/prefer-stateless-function
 const App = () => (
   <>
     <Navbar />
+    <HomePage />
     <Calculator />
   </>
 );
